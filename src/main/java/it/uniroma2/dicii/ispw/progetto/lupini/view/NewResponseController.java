5ca25667e3f9cb3e28debfafff779ec83f912df2
Martina <martinalupini11@gmail.com>
2022-12-23 19:31:53 +0200

    Fine interfaccia grafica utente con aggiunta classi mancanti

---
+++ src/main/java/it/uniroma2/dicii/ispw/progetto/lupini/view/NewResponseController.java
@@ -0,0 +1,20 @@
+package it.uniroma2.dicii.ispw.progetto.lupini.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class NewResponseController extends EmptyScreen{
+
+    @FXML
+    private Label originalQuestion;
+
+    @FXML
+    private TextField responseText;
+
+    @FXML
+    public void submitResponse(ActionEvent event) {
+
+    }
+}
